f6f3ab7b02eea6907da85e2e7fccf8d8ef606542
Andrew Jo <andrew@verdigris.co>
2018-08-10 16:42:40 -0700

    Autoload controllers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import EventEmitter from "events";
 import express from "express";
+import fs from "fs-extra";
+import path from "path";
+
 import routes from "./app/routes";
 import Config from "./config";
+import Controller from "./controller";
 
 export default class App extends EventEmitter {
   protected app: any;
   protected config: Config;
   protected server: any;
+  protected controllers: Map<string, Controller>;
 
   constructor(config: Config) {
     super();
@@ -16,7 +21,24 @@ export default class App extends EventEmitter {
     this.loadRoutes();
   }
 
-  private loadControllers() {
+  private async loadControllers() {
+    const controllerPath = path.join(__dirname, "app", "controllers");
+    const controllerFiles = await fs.readdir(controllerPath);
+
+    this.controllers = new Map();
+
+    const allControllers = await Promise.all(controllerFiles.filter(file => path.parse(file).ext !== ".map")
+      .map(async file => {
+        return {
+          name: path.parse(file).name,
+          cls: await import(path.join(controllerPath, file))
+        };
+      }));
+
+    allControllers.reduce((controllers, controller) => {
+      controllers.set(controller.name, controller.cls);
+      return controllers;
+    }, this.controllers);
   }
 
   private loadRoutes() {
