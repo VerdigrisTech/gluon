bd9e7ee86e781c7e3500975a138591ba82e81ae7
Andrew Jo <andrew@verdigris.co>
2018-08-14 11:28:53 -0700

    Raise error when child class does not implement

--- src/controller.ts
+++ src/controller.ts
@@ -1,3 +1,23 @@
 export default class Controller {
   constructor() {}
+
+  public index() {
+    throw new Error("Index method is not implemented");
+  }
+
+  public show() {
+    throw new Error("Show method is not implemented");
+  }
+
+  public create() {
+    throw new Error("Create method is not implemented");
+  }
+
+  public update() {
+    throw new Error("Update method is not implemented");
+  }
+
+  public destroy() {
+    throw new Error("Destroy method is not implemented");
+  }
 };
