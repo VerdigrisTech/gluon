2a00e677d5e822a89209d893bfe7fb0b4f4c30ea
Andrew Jo <andrew@verdigris.co>
2018-08-14 11:29:23 -0700

    Pluralize filename

--- src/app/controllers/pivotal_tracker/projects.ts
+++ src/app/controllers/pivotal_tracker/projects.ts
@@ -3,9 +3,11 @@ import request from "request-promise-native";
 
 import Controller from "../../../controller";
 
-export default class ProjectController extends Controller {
+export default class ProjectsController extends Controller {
   public async iterationAnalytics(req: Request, res: Response) {
     const projectId: string = req.params.projectId;
     const iteration: string = req.params.iteration;
   }
+
+  public async iterations(req, res) {}
 };
