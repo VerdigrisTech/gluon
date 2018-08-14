8eefca48a4942f892a1659850d7c4041384817e3
Andrew Jo <andrew@verdigris.co>
2018-08-14 15:04:03 -0700

    Remove trailing slash in base endpoint URL

--- config/pivotal-tracker.js
+++ config/pivotal-tracker.js
@@ -4,5 +4,5 @@ const process = require('process');
 
 module.exports = {
   apiToken: process.env.PIVOTAL_TRACKER_API_TOKEN,
-  apiEndpoint: process.env.PIVOTAL_TRACKER_API_ENDPOINT || 'https://www.pivotaltracker.com/services/v5/'
+  apiEndpoint: process.env.PIVOTAL_TRACKER_API_ENDPOINT || 'https://www.pivotaltracker.com/services/v5'
 };
