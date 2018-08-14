fc252ef4d5c0b7992b3dcb93a3677116d6b00b5e
Andrew Jo <andrew@verdigris.co>
2018-08-14 15:04:36 -0700

    Implement index and show routes

--- src/app/controllers/pivotal_tracker/projects.ts
+++ src/app/controllers/pivotal_tracker/projects.ts
@@ -1,13 +1,46 @@
 import { Request, Response } from "express";
 import request from "request-promise-native";
 
-import Controller from "../../../controller";
+import Config from "@gluon/config";
+import Controller from "@gluon/controller";
 
 export default class ProjectsController extends Controller {
-  public async iterationAnalytics(req: Request, res: Response) {
-    const projectId: string = req.params.projectId;
-    const iteration: string = req.params.iteration;
+  private get baseRequestUrl(): string {
+    return Config.get("pivotal-tracker.apiEndpoint");
   }
 
-  public async iterations(req, res) {}
+  private get requestOptions(): any {
+    return {
+      headers: {
+        "X-TrackerToken": Config.get("pivotal-tracker.apiToken")
+      }
+    };
+  };
+
+  public async index(req: Request, res: Response) {
+    const apiEndpoint = `${this.baseRequestUrl}/projects`;
+    const response = await request(apiEndpoint, this.requestOptions);
+    const projects = JSON.parse(response);
+    const attachments = projects.map(project => {
+      return {
+        title: project.name,
+        title_link: `https://www.pivotaltracker.com/n/projects/${project.id}`
+      };
+    });
+
+    res.json({ attachments });
+  }
+
+  public async show(req: Request, res: Response) {
+    const projectId = req.params.projectId;
+    const apiEndpoint = `${this.baseRequestUrl}/projects/${projectId}`;
+    const response = await request.get(apiEndpoint, this.requestOptions);
+    const project = JSON.parse(response);
+    const attachments = {
+      title: project.name,
+      title_link: `https://www.pivotaltracker.com/n/projects/${project.id}`
+    };
+
+    res.json({ attachments });
+  }
 };
