9889badc01e976516b502db9394ac89749eb8558
Andrew Jo <andrew@verdigris.co>
2018-08-14 11:30:42 -0700

    Load controllers and routes

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,63 @@
 import EventEmitter from "events";
-import express from "express";
-import fs from "fs-extra";
+import express, { Express, RequestHandler } from "express";
+import { last } from "lodash";
 import path from "path";
 
 import routes from "./app/routes";
-import Config from "./config";
-import Controller from "./controller";
+import Config from "@gluon/config";
+import Controller from "@gluon/controller";
+import { Route } from "@gluon/router";
+import importTree from "@gluon/utils/import-tree";
 
 export default class App extends EventEmitter {
-  protected app: any;
+  protected app: Express;
   protected config: Config;
   protected server: any;
   protected controllers: Map<string, Controller>;
+  protected controllerPath: string = path.join(__dirname, "app", "controllers");
 
   constructor(config: Config) {
     super();
     this.config = config;
     this.app = express();
-    this.loadControllers();
+    this.initialize();
+  }
+
+  private async initialize() {
+    await this.loadControllers();
     this.loadRoutes();
   }
 
   private async loadControllers() {
-    const controllerPath = path.join(__dirname, "app", "controllers");
-    const controllerFiles = await fs.readdir(controllerPath);
-
+    const controllerClasses = await importTree(this.controllerPath);
     this.controllers = new Map();
-
-    const allControllers = await Promise.all(controllerFiles.filter(file => path.parse(file).ext !== ".map")
-      .map(async file => {
-        return {
-          name: path.parse(file).name,
-          cls: await import(path.join(controllerPath, file))
-        };
-      }));
-
-    allControllers.reduce((controllers, controller) => {
-      controllers.set(controller.name, controller.cls);
+    controllerClasses.reduce((controllers: Map<string, Controller>, controller: { path: string, module: any }) => {
+      const ControllerClass = controller.module.default;
+      const controllerDir = path.dirname(controller.path);
+      const controllerName = path.parse(controller.path).name;
+      const controllerPath = path.join(controllerDir, controllerName);
+      const relativePath = path.relative(this.controllerPath, controllerPath);
+      controllers.set(path.join("/", relativePath), new ControllerClass());
       return controllers;
     }, this.controllers);
   }
 
   private loadRoutes() {
+    const rootRoute = new Route("/", "GET");
+    routes(rootRoute);
+    const allRoutes = rootRoute.list().filter(route => route.controller);
+    allRoutes.forEach(route => {
+      const routeMethod: (path: string, ...callback: RequestHandler[]) => Express = this.app[route.verb.toLowerCase()].bind(this.app);
+      // Resolve controller methods
+      if (typeof route.controller === "function") {
+        routeMethod(route.path, route.controller);
+      } else if (typeof route.controller === "string") {
+        const methodName = last(route.controller.split("."));
+        const controller = this.controller(route.path);
+        const controllerMethod = controller[methodName];
+        routeMethod(route.path, controllerMethod);
+      }
+    });
   }
 
   public address() {
@@ -53,5 +69,10 @@ export default class App extends EventEmitter {
     const _listenAddr = listenAddr || this.config.get("server.listenAddr");
     this.server = this.app.listen(_port, _listenAddr);
     this.server.on("listening", () => this.emit("listening"))
+  }
+
+  public controller(routePath: string): Controller {
+    const key = routePath.replace(/\/:[\w\-]+/g, "");
+    return this.controllers.get(key);
   }
 };
